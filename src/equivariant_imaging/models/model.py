40d0597ee32ffec217736de5e5a8963c15badbe7
Aurian <auriankelen@gmail.com>
2022-02-03 11:59:41 +0100

    Adding the validation dataloader

--- src/equivariant_imaging/models/model.py
+++ src/equivariant_imaging/models/model.py
@@ -80,13 +80,16 @@ class EI(pl.LightningModule):
     
     def train_dataloader(self):
         # transforms
-        # prepare transforms standard to MNIST
+        # prepare transforms standard to MNIST by passing the mean and std deviation
         transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
         # data
         mnist_train = MNIST('./data/', train=True, download=True, transform=transform)
         return DataLoader(mnist_train, batch_size = 64)
     
-
+    def val_dataloader(self):
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        mnist_val =  MNIST('./data/', train=False, download=True, transform=transform)
+        return DataLoader(mnist_val, batch_size=64)
 
 if __name__ == "__main__":
     # get dataset
