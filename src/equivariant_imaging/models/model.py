3eaca3726b2093e13ea88fd8d916de024d140cf6
LoanSarazin <loan.sarazin@gmail.com>
2022-02-08 11:07:36 +0100

    Changing normalization

--- src/equivariant_imaging/models/model.py
+++ src/equivariant_imaging/models/model.py
@@ -75,12 +75,12 @@ class EI(pl.LightningModule):
         self.transform = transforms.Compose([
             transforms.Pad(2, padding_mode="edge"),
             transforms.ToTensor(),
-            transforms.Normalize((0.1307, ), (0.3081, ))
+            transforms.Normalize((0.5, ), (0.5, ))
         ])
 
         self.invtransform = transforms.Compose([
-            transforms.Normalize((0, ), (1 / 0.3081, )),
-            transforms.Normalize((-0.1307, ), (1, )),
+            transforms.Normalize((0, ), (1 / 0.5, )),
+            transforms.Normalize((-0.5, ), (1, )),
             transforms.CenterCrop(28)
         ])
 
